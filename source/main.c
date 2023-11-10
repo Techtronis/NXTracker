747c4e31b379da0031b7ff60879bae3877a5f4fc
Techtronis <108685009+Techtronis@users.noreply.github.com>
2023-11-10 20:55:23 +0100

    cant get button input to fucking work

--- source/main.c
+++ source/main.c
@@ -8,79 +8,50 @@ char conNames[9][99] = {"Nintendo Switch \n", "Wii U \n",
                         "Wii \n", "Gamecube \n", 
                         "Nintendo 64 \n", "Virtual Boy \n", 
                         "SNES \n", "NES \n"};
+                        
 
 int consoleSelectScreen(int y)
 {
-    while(appletMainLoop)
-    {
-        consoleInit(NULL);
-        consoleUpdate(NULL);
-        printf("NXTracker v0.0.1\n \n");
-        printf("console select");
+    consoleInit(NULL);
+    consoleUpdate(NULL);
+    printf("NXTracker v0.0.1\n \n");
+    printf("console select");
 
-        if(y == 0)
+    if(y == 0)
+    {
+        while(true)
         {
             printf("home consoles");
             consoleUpdate(NULL);
         }
-        else if (y == 1)
+    }
+    else if (y == 1)
+    {
+        while(true)
         {
             printf("handheld consoles");
             consoleUpdate(NULL);
         }
-        else if (y == 2)
+    }
+    else if (y == 2)
+    {
+        while(true)
         {
             printf("current consoles");
             consoleUpdate(NULL);
         }
     }
-
-    return 0;
-}
-
-int categorySelectScreen()
-{
-    while(appletMainLoop)
-    {
-        consoleInit(NULL);
-        printf("NXTracker v0.0.1\n \n");
-        printf("Categories: \n\n [A] Home Consoles \n\n [B] Handheld Consoles [X] Current Consoles");
-
-        padConfigureInput(1, HidNpadStyleSet_NpadStandard);
-        PadState pad;
-        padInitializeDefault(&pad);
-        u64 kDown = padGetButtonsDown(&pad);
-
-        if(kDown & HidNpadButton_A)
-        {
-            consoleSelectScreen(0);
-            consoleUpdate(NULL);
-        }
-        else if (kDown & HidNpadButton_B)
-        {
-            consoleSelectScreen(1);
-            consoleUpdate(NULL);
-        }
-        else if (kDown & HidNpadButton_X)
-        {
-        consoleSelectScreen(2);
-        consoleUpdate(NULL);
-        }
     
-    }
+
     return 0;
 }
 
 int main(int argc, char* argv[])
 {
-
     int x = 0;
-    
-
-    // Configure our supported input layout: a single player with standard controller styles
-    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
 
     // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
+    
     PadState pad;
     padInitializeDefault(&pad);
     consoleInit(NULL);
@@ -92,44 +63,41 @@ int main(int argc, char* argv[])
     while (appletMainLoop())
     {
         // Scan the gamepad. This should be done once for each frame
+        u64 kDown = padGetButtonsDown(&pad);
         padUpdate(&pad);
-
-        categorySelectScreen();
 
         // padGetButtonsDown returns the set of buttons that have been
         // newly pressed in this frame compared to the previous one
-        u64 kDown = padGetButtonsDown(&pad);
+        
+        printf("NXTracker v0.0.1\n \n");
+        printf("Categories: \n\n [A] Home Consoles \n\n [B] Handheld Consoles [X] Current Consoles");
 
-        /*if (kDown & HidNpadButton_Right)
+        padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+        if(kDown & HidNpadButton_Down)
         {
-            consoleInit(NULL);
-            if(x != 8){
-                printf("NXTracker v0.0.1\n \n ");
-                x = x + 1;
-                printf(conNames[x]);
-            } else {
-                printf("NXTracker v0.0.1\n \n ");
-                x = 0;
-                printf(conNames[x]);
-            }
+            consoleSelectScreen(0);
+            consoleUpdate(NULL);
+            printf("pressed");
+        }
+        else if (kDown & HidNpadButton_Right)
+        {
+            consoleSelectScreen(1);
+            consoleUpdate(NULL);
+            printf("pressed");
         }
         else if (kDown & HidNpadButton_Left)
-        { 
-            consoleInit(NULL);
-            if(x != -1){
-                printf("NXTracker v0.0.1\n \n ");
-                x = x - 1;
-                printf(conNames[x]);
-            } else {
-                printf("NXTracker v0.0.1\n \n ");
-                x = 7;
-                printf(conNames[x]);
-            }
-        }*/
+        {
+            consoleSelectScreen(2);
+            consoleUpdate(NULL);
+            printf("pressed");
+        }
+        
+    }
         
         // Update the console, sending a new frame to the display
         consoleUpdate(NULL);
-    }
+    
 
     // Deinitialize and clean up resources used by the console (important!)
     consoleExit(NULL);
