8f50467b4413bf4adeca7c07cc15104593560435
Techtronis <108685009+Techtronis@users.noreply.github.com>
2023-11-12 21:59:23 +0100

    im tryin to put in a cursor, i cant, pls help me

--- source/main.c
+++ source/main.c
@@ -3,16 +3,11 @@ Made by: Alexis Jost
 Started on: November 3rd 2023
 */
 
-// Include the most common headers from the C standard library
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
 #include <switch.h>
-
-/*
-    Store the cursor in a line array and make it move different array lines
-*/
 
 //      Home Consoles
 char homeConNames[8][99] = {"Wii U \n",
@@ -20,24 +15,14 @@ char homeConNames[8][99] = {"Wii U \n",
                             "Nintendo 64 \n", "Virtual Boy \n",
                             "SNES \n", "NES \n"};
 //      Handheld Devices
-char handConNames[11][99] = {"Game & Watch", "Game Boy",
-                             "Game Boy Color", "Game Boy Advance",
-                             "Game Boy Advance SP", "Game Boy Micro",
-                             "Nintendo DS", "Nintendo DS Lite",
-                             "Nintendo DSi (XL)", "(New) Nintendo 3DS (XL)",
-                             "(New) Nintendo 2ds"};
+char handConNames[11][99] = {"Game & Watch \n", "Game Boy \n",
+                             "Game Boy Color \n", "Game Boy Advance \n",
+                             "Game Boy Advance SP \n", "Game Boy Micro \n",
+                             "Nintendo DS \n", "Nintendo DS Lite \n",
+                             "Nintendo DSi (XL) \n", "(New) Nintendo 3DS (XL) \n",
+                             "(New) Nintendo 2ds \n"};
 //      Current Consoles
-char currConNames[3][99] = {"Nintendo Switch", "Nintendo Switch Lite", "Nintendo Switch OLED model"};
-
-int cursor(int x)
-{
-    char cursorPointer[x][2];
-    strcpy(cursorPointer[x], ">");
-    strcpy(cursorPointer[x-1], " ");
-
-    printf(cursorPointer[x]);
-    return 0;
-}
+char currConNames[3][99] = {"Nintendo Switch \n", "Nintendo Switch Lite \n", "Nintendo Switch OLED model \n"};
 
 int consoleSelectScreen(int y)
 {
@@ -47,49 +32,50 @@ int consoleSelectScreen(int y)
     printf("NXTracker v0.0.1\n \n");
     printf("Choose a console \n \n");
 
-    if (y == 0)
+    switch(y)
     {
-        for (int i = 0; i <= 8; i++)
-        {
-            printf(homeConNames[i], "\n \n");
-        }
-
-        consoleUpdate(NULL);
+        case 0:
+            for (int i = 0; i <= 8; i++)
+            {
+                printf(homeConNames[i]);
+            }
+            consoleUpdate(NULL);
+            break;
+        case 1:
+            for (int i = 0; i <= 11; i++)
+            {
+                printf(handConNames[i]);
+            }
+            consoleUpdate(NULL);
+            break;
+        case 2:
+            for (int i = 0; i <= 3; i++)
+            {
+                printf(currConNames[i]);
+            }
+            consoleUpdate(NULL);
+            break;
+        default:
+            printf("NXTracker v0.1\n \n");
+            printf("Categories: \n\n [A] Home Consoles \n\n [B] Handheld Consoles [X] Current Consoles");
+            break;
     }
-    else if (y == 1)
-    {
-        for (int i = 0; i <= 11; i++)
-        {
-            printf(handConNames[i]);
-            printf("\n \n");
-        }
-        consoleUpdate(NULL);
-    }
-    else if (y == 2)
-    {
-        for (int i = 0; i <= 3; i++)
-        {
-            printf(currConNames[i]);
-            printf("\n \n");
-        }
-        consoleUpdate(NULL);
-    }
-
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
-
     // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
-
     PadState pad;
     padInitializeDefault(&pad);
     consoleInit(NULL);
 
-    printf("NXTracker v0.1\n \n");
-    printf("Categories: \n\n [A] Home Consoles \n\n [B] Handheld Consoles [X] Current Consoles");
+    char cursor[11][2];
 
+    printf("NXTracker v0.1\n \n");
+
+    printf("Categories: \n\n [A] Home Consoles \n\n [B] Handheld Consoles [X] Current Consoles \n \n \n");
+    
     // Main loop
     while (appletMainLoop())
     {
@@ -98,6 +84,16 @@ int main(int argc, char *argv[])
         padUpdate(&pad);
         padConfigureInput(1, HidNpadStyleSet_NpadStandard);
 
+        if (kDown & HidNpadButton_Down)
+        {
+            for(int i = 0; i <= 11; i++)
+            {
+                strcpy(cursor[i], "");
+                printf(cursor[i]);
+                printf("\n"); 
+            }
+            printf("\033[12A");
+        }
         if (kDown & HidNpadButton_A)
         {
             consoleSelectScreen(0);
